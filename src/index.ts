9e475b4b7eaa56a273135762d7970fd4d45beb1d
Vaylots <Vaylots@gmail.com>
2023-01-20 08:04:33 +0200

    remove database module

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,12 @@
 import { config } from "dotenv";
 import { OpenAICompletion } from "./modules/OpenAiCompletion";
-import { DatabaseController } from "./modules/DatabaseModule";
 import { Telegraf, Markup } from "telegraf";
 config();
 
-const database = new DatabaseController();
 const openai = new OpenAICompletion();
 const bot = new Telegraf(`${process.env.BOT_TOKEN}`);
 
 bot.command("start", async (ctx) => {
-  const joinDate: string = new Date().toLocaleDateString();
-  database.addUser(
-    ctx.message.from.username,
-    ctx.message.from.id.toString(),
-    joinDate
-  );
   const message = `Привет, ${ctx.message.from.username}, я бот который поможет тебе опробовать chatGPT без зарубежного номера для регистрации. Чтобы воспользоваться ботом напиши команду "/chat <текст>", пример "/chat что такое ChatGPT". `;
   await ctx.reply(message);
 });
@@ -37,19 +29,6 @@ bot.command("chat", async (ctx) => {
     await ctx.reply(
       "Простите произошла ошибка, попробуйте ещё раз.\nЕсли ошибка не пропала, напишите сюда.\nt.me/vaylots"
     );
-  }
-});
-
-bot.command("users", async (ctx) => {
-  if (ctx.message.from.id.toString() == "586694681") {
-    const users = database.showAllUsers(ctx.message.from.id.toString());
-    users.then((users) => {
-      users?.forEach((user) => {
-        ctx.reply(
-          `id:${user.id}\nusername: ${user.username}\ntelegram_id: ${user.telegram_id}\njoin_date: ${user.join_date}`
-        );
-      });
-    });
   }
 });
 
