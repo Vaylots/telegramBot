fdbb8cb7ec02e7aa38835a978f5126f97c2596dc
Vaylots <Vaylots@gmail.com>
2023-01-22 06:13:19 +0200

    did some refactoring and added comments

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ const openai = new OpenAICompletion();
 const bot = new Telegraf(`${process.env.BOT_TOKEN}`);
 const languageDetector = new LanguageDetect();
 
+// Welcome the user to the bot and provide instructions on how to use it!
 bot.command("start", async (ctx) => {
   const message = `Привет, ${ctx.message.from.username}, я бот который поможет тебе опробовать chatGPT без зарубежного номера для регистрации. Чтобы воспользоваться ботом напиши команду "/chat <текст>", пример "/chat что такое ChatGPT". `;
   await ctx.reply(message);
@@ -15,14 +16,25 @@ bot.command("start", async (ctx) => {
 
 bot.command("chat", async (ctx) => {
   try {
+    // Splitting the message into an array and removing the first element
     let message: string[] = ctx.message.text.split(" ");
     message.splice(0, 1);
+    // This line joins the elements of the prompts array into a single prompt string, separated by spaces.
     const prompts: string = message.join(" ");
+    // If there are prompts, the bot sends a request to the open ai api and then sends the response to the user.
     if (prompts) {
+      //  Assigning the message_id of the reply to the waitMessageId variable for deleting in future
       const waitMessageId = (await ctx.reply("Пожалуйста подождите"))
         .message_id;
+      // This line is awaiting a response from the openai API based on the prompts provided
       const response = (await openai.getCompletion(`${prompts}`)).text;
+      // This line is detecting the language of the response variable and assigning it to the codeLanguage variable
       const codeLanguage = languageDetector.detectLanguage(`${response}`);
+      /* 
+        This code checks if the code language is not "Natural" and if so, it will try to reply with a markdown version of the response. 
+        If it fails, it will reply with the response without markdown. 
+        If the code language is "Natural", it will reply with the response without markdown.
+      */
       if (codeLanguage != "Natural") {
         try {
           await ctx.replyWithMarkdownV2(
@@ -34,11 +46,17 @@ bot.command("chat", async (ctx) => {
       } else {
         await ctx.reply(`${response}`);
       }
+      // This line deletes a message from the chat with the given chat ID and message ID.
       bot.telegram.deleteMessage(ctx.chat.id, waitMessageId);
     } else {
       await ctx.reply("Похоже вы не ввели текст");
     }
   } catch (error) {
+    /*
+    If an error occurs in processing the command, 
+    output the error to the console and send the user a message 
+    asking them to reformulate the request and the Telegram of the author for communication.
+    */
     console.log(error);
     await ctx.reply(
       "Простите, произошла ошибка, пожалуйста переформулируйте свой запрос и попробуйте ещё раз.\nЕсли ошибка не пропала, напишите сюда.\nt.me/vaylots"
@@ -46,6 +64,7 @@ bot.command("chat", async (ctx) => {
   }
 });
 
+// This command provides a link to the author's portfolio.
 bot.command("author", async (ctx) => {
   ctx.reply(
     `Тут можно найти автора`,
--- src/modules/OpenAiCompletion.ts
+++ src/modules/OpenAiCompletion.ts
@@ -5,12 +5,16 @@ export class OpenAICompletion {
   private openai: OpenAIApi;
 
   constructor() {
+    // Initializing the Configuration object with the API key from the environment variable OPENAI_API_KEY.
     this.configuration = new Configuration({
       apiKey: `${process.env.OPENAI_API_KEY}`,
     });
+    // Initializes a new OpenAIApi instance with the given configuration
     this.openai = new OpenAIApi(this.configuration);
   }
+  //This function uses the OpenAI API to generate a completion for the given prompt, with parameters and return the choices from OpenAI response.
   async getCompletion(prompt: string) {
+    // This code is making an API call to the OpenAI API to create a completion using the given parameters.
     const response = await this.openai.createCompletion({
       model: "text-davinci-003",
       prompt: `${prompt}`,
@@ -20,6 +24,7 @@ export class OpenAICompletion {
       frequency_penalty: 0,
       presence_penalty: 0,
     });
+
     return response.data.choices[0];
   }
 }
